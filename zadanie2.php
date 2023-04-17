f8f68259e342338fea6fe9b2d37552c2b018e04f
JS0Npl <130741801+JS0Npl@users.noreply.github.com>
2023-04-17 12:02:18 +0200

    Add files via upload

---
+++ zadanie2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Zadanie 2</title>
+</head>
+<body>
+    <form method="get" action="">
+      <p> Login: </p> <input type="text" value="admin" name="login" id=""> <br>
+       <p> Hasło </p> <input type="password" value="test" name="haslo" id=""> <br>
+        <input type="submit" name="submit" value="Zaloguj się">
+    </form>
+</body>
+</html>
+
+<?php
+if (isset($_GET['submit'])) {
+    if($_GET['login'] === "admin") {
+        if($_GET['haslo'] === "test") {
+            echo "Witaj, ".$_GET['login'];
+        }
+        else {
+            echo "<p> Nieprawidłowe hasło! <p>";
+        }
+    } else {
+        echo"<p> Nieprawidłowy login! <p>";
+    }
+}
+?>
