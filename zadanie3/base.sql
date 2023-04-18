53565023a676b369c4936176593ea652cb378006
JS0Npl <130741801+JS0Npl@users.noreply.github.com>
2023-04-18 15:33:22 +0200

    Delete base.sql

--- zadanie3/base.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE Users (ID INT NOT NULL PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE (username));
-INSERT INTO `Users` (`ID`, `username`, `password`) VALUES (NULL, 'admin', 'test')
