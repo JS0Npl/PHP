941de60e835084b0e213df5e1b2ef18e1de0eadf
JS0Npl <130741801+JS0Npl@users.noreply.github.com>
2023-04-19 06:57:28 +0200

    Update index.php

--- zadanie3/index.php
+++ zadanie3/index.php
@@ -6,16 +6,16 @@
     <title>Zadanie 2</title>
 </head>
 <body>
-    <form method="get" action="">
-      <p> Login: </p> <input type="text" value="admin" name="username" id=""> <br>
-       <p> Hasło </p> <input type="password" value="test" name="password" id=""> <br>
+    <form method="POST" action="">
+      <p> Username: </p> <input type="text" value="admin" name="username" id=""> <br>
+       <p> Password: </p> <input type="password" value="test" name="password" id=""> <br>
         <input type="submit" name="submit" value="Log in">
     </form>
 </body>
 </html>
 
 <?php
-
+/*
 $base = new mysqli("localhost", "root", "", "base");
 
 if($base->connect_error){
@@ -33,6 +33,35 @@ if (isset($_POST['submit'])) {
     }
     else {
         echo "<p>Incorrect password or login.</p>";
+    }
+}
+*/
+?>
+
+<?php
+$db = new mysqli("localhost", "root", "", "base");
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+    $stmt = $db->prepare($query);
+
+    $stmt->bind_param("ss", $username, $password);
+
+    $stmt->execute();
+
+    $stmt->store_result();
+
+
+    if($stmt->num_rows == 1) {
+        session_start();
+        $_SESSION['username'] = $username;
+        header("location: index.php");
+    } else {
+        echo "Wrong username or password";
     }
 }
 ?>
